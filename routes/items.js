ad0ea54b69c617518f25eaee64dea1d11c6474ad
Isaac Shea <isaac_shea@icloud.com>
2022-05-13 21:51:49 +1000

    Updated for 22w05a

--- routes/items.js
+++ routes/items.js
@@ -15,6 +15,16 @@ const itemTypes = require('../json/types.json');
 
 
 // FUNCTIONS ---------------------------------------------------------------------------
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+
+
+// FUNCTIONS ---------------------------------------------------------------------------
 /** Creates items with the given parameters
  * @param {client} client a registered PostgreSQL object
  * @param {String} name name of the item
@@ -42,7 +52,7 @@ async function createItems(client, name, description, rarityID, typeID, amount, 
     // Create the item/s
     var returnIDs = [];
     for (var i = 0; i < amount; i++) {
-        var query = 'INSERT INTO items (name, description, rarity_id, type_id, owner_id, attributes) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;';
+        var query = 'INSERT INTO items (name, description, rarity_id, type_id, owner_id, attributes) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id;';
         var params = [ name, description, rarityID, typeID, ownerID, JSON.stringify(attributes) ];
         var err, result = await client.query(query, params);
         if (err) { return [ false, 'Error occurred while creating' ]; }
@@ -56,10 +66,11 @@ async function createItems(client, name, description, rarityID, typeID, amount, 
 
 /** Transfers ownership of an item from it's original owner to the new owner
  * @param {client} client a registered PostgreSQL object
- * @param {UUID} itemID server-generated ID for the item to transfer
+ * @param {String/UUID} itemID server-generated item instance ID
  * @param {DiscordID} newOwnerID Discord-generated ID of the user to transfer this item to
+ * @param {Boolean} isStack whether to transfer any stackable items owned by this user
  */
-async function transferItem(client, itemID, newOwnerID) { 
+async function transferItems(client, itemID, newOwnerID, isStack) { 
 
     // Check if that item exists
     var query = 'SELECT * FROM items WHERE id = $1;';
@@ -74,41 +85,114 @@ async function transferItem(client, itemID, newOwnerID) {
     var err, result = await client.query(query, params);
     if (err || result.rows.length === 0) { return [ false, 'This user does not exist' ]; }
 
-    var query = 'UPDATE items SET owner_id = $1 WHERE id = $2;';
-    var params = [ newOwnerID, itemID ];
-    var err, result = await client.query(query, params);
-    if (err) { return [ false, 'An error occurred while transferring item' ]; }
+    // Check for a stack of items (that aren't dropped)
+    if (isStack === 'true') {
+        var query = 'SELECT * FROM items WHERE name = $1 AND owner_id = $2 AND is_dropped = $3;';
+        var params = [ itemInfo.name, itemInfo.owner_id, false ];
+        var err, result = await client.query(query, params);
+        if (err) { res.status(500).send('Internal server error'); }
 
-    console.log('Transferred item', itemID, 'to user', newOwnerID);
+        if (result.rows.length > 1) {   // If more than the one item can be found...
+            // Transfer all items
+            for (const item of result.rows) { 
+                var query = 'UPDATE items SET owner_id = $1 WHERE id = $2;';
+                var params = [ newOwnerID, item.id ];
+                var err, result = await client.query(query, params);
+                if (err) { res.status(500).send(err); console.log(err); return; }
+            }
+
+            return [ true, 'Successfully transferred items' ];
+        }
+    }
+
+    // Transfer all the items
+    for (const item of items) { 
+        var query = 'UPDATE items SET owner_id = $1 WHERE id = $2;';
+        var params = [ newOwnerID, item.id ];
+        var err, result = await client.query(query, params);
+        if (err) { return [ false, 'An error occurred while transferring item' ]; }
+    }
+
+    console.log('Transferred items to user ', newOwnerID);
     return [ true, 'Successfully transferred item' ];
 }
+
+/** Groups an array of objects by it's key value
+ * @param {Array} list list to sort
+ * @param {String} key key to sort by
+ */
+function groupArrayOfObjects(list, key) {
+    return list.reduce(function(rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+    }, {});
+};
 
 
 
 // ENDPOINTS ---------------------------------------------------------------------------
-router.get('/:id', async function (req, res) {                          // Get item by ID
+router.get('/:id/:isStack', async function (req, res) {                 // Get item by ID
     const client = req.client;
 
-    var query = 'SELECT  * FROM items WHERE id = $1;';
+    // Retrieve the item's information
+    var query = 'SELECT * FROM items WHERE id = $1;';
     var params = [ req.params.id ];
     var err, result = await client.query(query, params);
     if (err) { res.status(500).send('Internal server error'); }
     if (result.rows.length === 0) { res.status(404).send('Nothing was found'); }
+    const itemInfo = result.rows[0];
 
-    // Assemble object
+    // Get a full stack of similar items
+    if (req.params.isStack === 'true') {
+        var query = 'SELECT * FROM items WHERE name = $1 AND owner_id = $2;';
+        var params = [ itemInfo.name, itemInfo.owner_id ];
+        var err, result = await client.query(query, params);
+        if (err) { res.status(500).send('Internal server error'); }
+
+        if (result.rows.length > 1) {   // More than just the one item can be found...
+
+            // Collate these items by whether they've been dropped
+            const itemStacks = groupArrayOfObjects(result.rows, 'is_dropped');
+
+            const itemList = [];
+            for (const item of Object.values(itemStacks)) {
+                const itemInfo = {
+                    id: item[0].id,
+                    ownerID: item[0].owner_id,
+                    name: item[0].name,
+                    description: item[0].description,
+                    type: itemTypes.find(x => x.id == item[0].type_id),
+                    rarity: itemRarities.find(x => x.id == item[0].rarity_id),
+                    amount: item.length,
+                    isEquipped: item[0].is_equipped,
+                    isDropped: item[0].is_dropped,
+                    attributes: item[0].attributes
+                }
+
+                itemList.push(itemInfo);
+            }
+
+            res.status(200).send(JSON.stringify(itemList));
+            return;
+        }
+    } 
+
+    // Either isStack is false, or no stackable items could be found
+    // Assemble single item object
     const item = {
-        id: result.rows[0].id,
-        name: result.rows[0].name,
-        description: result.rows[0].description,
-        ownerID: result.rows[0].owner_id,
-        rarity: itemRarities.find(item => item.id === result.rows[0].rarity_id),
-        type: itemTypes.find(item => item.id === result.rows[0].type_id),
-        attributes: result.rows[0].attributes,
-        isEquipped: result.rows[0].is_equipped,
-        isDropped: result.rows[0].is_dropped,
+        id: itemInfo.id,
+        name: itemInfo.name,
+        description: itemInfo.description,
+        ownerID: itemInfo.owner_id,
+        type: itemTypes.find(item => item.id === itemInfo.type_id),
+        rarity: itemRarities.find(item => item.id === itemInfo.rarity_id),
+        amount: 1,
+        attributes: itemInfo.attributes,
+        isEquipped: itemInfo.is_equipped,
+        isDropped: itemInfo.is_dropped,
     }
 
-    res.status(200).send(JSON.stringify(item));
+    res.status(200).send(JSON.stringify([ item ]));
 });
 
 router.post('/create/', async function (req, res) {                     // Create an item
@@ -136,26 +220,8 @@ router.post('/create/', async function (req, res) {                     // Creat
 
     if (success === false) { res.status(500).send(response); console.log(response); return; } 
 
-    // Return ID
+    // Return IDs
     res.status(200).send(JSON.stringify({ itemIDs: response }));
-});
-
-router.post('/:id/transfer/:newOwnerID/', async function (req, res) {   // Transfer an item
-    const client = req.client;
-
-    const transferInfo = {
-        itemID: req.params.id,
-        newOwnerID: req.params.newOwnerID
-    }
-
-    if (Object.values(transferInfo).every(x => x === null || x === '')) { 
-        res.status(400).send('Not all required values were provided'); return;
-    }
-
-    const [ success, response ] = await transferItem(client, transferInfo.itemID, transferInfo.newOwnerID);
-    if (success === false) { res.status(500).send(response); console.log(response); return; } 
-
-    res.status(200).send('Item transferred successfully');
 });
 
 router.post('/:id/equip/:isEquipping/', async function (req, res) {     // Equip/unequip item
@@ -185,11 +251,12 @@ router.post('/:id/equip/:isEquipping/', async function (req, res) {     // Equip
     res.status(200).send('Item equipped/unequipped successfully');
 });
 
-router.post('/:id/drop/:isDropping/', async function (req, res) {       // Drop/pickup item
+router.post('/:id/drop/:isDropping/:isStack', async function (req, res) {   // Drop/pickup items
     const client = req.client;
 
     const itemID = req.params.id;
     const isDropping = req.params.isDropping;
+    const isStack = req.params.isStack || false;    // Value is false by default
     if (!itemID || !isDropping) { res.status(400).send('Missing itemID and/or isDropping values'); return; }
 
     // Check if the item exists
@@ -198,14 +265,54 @@ router.post('/:id/drop/:isDropping/', async function (req, res) {       // Drop/
     var err, result = await client.query(query, params);
     if (err) { res.status(500).send(err); console.log(err); return; }
     if (result.rows.length === 0) { res.status(404).send('An item with that ID does not exist'); return; }
+    const itemInfo = result.rows[0];
 
-    // Drop/pickup item
+    // Check for a stack of items
+    if (isStack === 'true') {
+        var query = 'SELECT * FROM items WHERE name = $1 AND owner_id = $2;';
+        var params = [ itemInfo.name, itemInfo.owner_id ];
+        var err, result = await client.query(query, params);
+        if (err) { res.status(500).send('Internal server error'); }
+
+        if (result.rows.length > 1) {   // If more than the one item can be found...
+            // Drop/pickup all items
+            for (const item of result.rows) { 
+                var query = 'UPDATE items SET is_dropped = $1 AND is_equipped = $2 WHERE id = $3;';
+                var params = [ isDropping, false, item.id ];
+                var err, result = await client.query(query, params);
+                if (err) { res.status(500).send(err); console.log(err); return; }
+            }
+
+            return;
+        }
+    }
+
+    // Drop/pickup single item
     var query = 'UPDATE items SET is_dropped = $1 AND is_equipped = $2 WHERE id = $3;';
     var params = [ isDropping, false, itemID ];
     var err, result = await client.query(query, params);
     if (err) { res.status(500).send(err); console.log(err); return; }
 
-    res.status(200).send('Item dropped/picked up successfully');
+    res.status(200).send('Item/s dropped/picked up successfully');
+});
+
+router.post('/:id/transfer/:newOwnerID/:isStack', async function (req, res) {   // Transfer items
+    const client = req.client;
+
+    const transferInfo = {
+        itemID: req.params.id,
+        newOwnerID: req.params.newOwnerID,
+        isStack: req.params.isStack || false
+    }
+
+    if (Object.values(transferInfo).every(x => x === null || x === '')) { 
+        res.status(400).send('Not all required values were provided'); return;
+    }
+
+    const [ success, response ] = await transferItems(client, transferInfo.itemID, transferInfo.newOwnerID, transferInfo.isStack);
+    if (success === false) { res.status(500).send(response); console.log(response); return; } 
+
+    res.status(200).send('Item/s transferred successfully');
 });
 
 
